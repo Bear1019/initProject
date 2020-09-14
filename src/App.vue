fb96e51be73ac4022b4a475e3e4d36f7979561bb
Bear1019 <766524514@qq.com>
2020-09-14 12:39:07 +0800

    frist

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app">
+    <router-link to='/login'><a>Login</a></router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<style lang="scss">
+#app {}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+<div id='login'>
+this is a login page
+</div>
+</template>
+
+<script>
+export default {
+
+    }
+</script>
+
+<style lang="" scoped>
+</style>
